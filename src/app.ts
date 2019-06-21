3e42a237d6c836f6ea25003fa0a96a7a315ca387
jaspesoft <jaspesoft@gmail.com>
2019-06-21 09:29:02 -0400

    elimando el server express, no va a internet esta app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,9 @@
-import * as express from 'express';
 import * as cron from 'node-cron';
 import { connect } from 'mongoose';
 import { environment } from './config/config';
 import { ONX } from './apps/onx';
 
-const port = 5050;
-const app = express();
-
 connect('mongodb://'+ environment.database.user +':'+ environment.database.pass +'@'+ environment.database.srv +':'+ environment.database.port +'/' + environment.database.db , {useNewUrlParser: true}).then(() => {
-    app.listen( port, () => {
-        console.log( `server BLOCKCHAIN ONX started at http://localhost:${ port }` );
-    });
-
     cron.schedule('*/2  *  *  *  *', () => {
         console.log('Sincronizando red onixcoin');
         ONX();
